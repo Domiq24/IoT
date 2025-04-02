849dfc8f516340ef3548b73a4a734d28d35dc1ed
Domiq24 <dominikpolitylo@gmail.com>
2025-04-02 08:34:03 +0200

    Update config.ts

--- IoT-dashboard/app/lib/config.ts
+++ IoT-dashboard/app/lib/config.ts
@@ -2,5 +2,5 @@ import * as process from "node:process";
 
 export const config = {
     port: process.env.PORT || 3100,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://test_user:test_passwd123@cluster0.lybvqum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    databaseUrl: process.env.MONGODB_URI || ''
 };
