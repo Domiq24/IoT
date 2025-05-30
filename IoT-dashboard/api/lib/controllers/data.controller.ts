5747eca4b639ba468ed02237e79e0b82dfd4d60b
Dominik <dominikpolitylo@gamil.com>
2025-05-30 07:03:00 +0200

    Lab10

--- IoT-dashboard/api/lib/controllers/data.controller.ts
+++ IoT-dashboard/api/lib/controllers/data.controller.ts
@@ -2,9 +2,11 @@ import Controller from '../interfaces/controller.interface';
 import { Request, Response, NextFunction, Router } from 'express';
 import { checkIdParam } from "../middlewares/deviceIdParam.middleware";
 import DataService from "../modules/services/data.service";
+import { IData } from "../modules/models/data.model";
 import { config } from "../config";
+import { admin } from "../middlewares/admin.middleware";
+import Joi from "joi";
 
-let testArr = [4,5,6,3,5,3,7,5,13,5,6,4,3,6,3,6];
 class DataController implements Controller {
     public path = '/api/data';
     public router = Router();
@@ -16,12 +18,12 @@ class DataController implements Controller {
 
     private initializeRoutes() {
         this.router.get(`${this.path}/latest`, this.getLatestReadingsFromAllDevices);
-        this.router.post(`${this.path}/:id`, checkIdParam, this.addData);
+        this.router.post(`${this.path}/:id`, admin, checkIdParam, this.addData);
         this.router.get(`${this.path}/:id`, checkIdParam, this.getAllDeviceData);
         this.router.get(`${this.path}/:id/latest`, checkIdParam, this.getPeriodData);
         this.router.get(`${this.path}/:id/:num`, checkIdParam, this.getPeriodData)
-        this.router.delete(`${this.path}/all`, this.cleanAllDevices);
-        this.router.delete(`${this.path}/:id`, checkIdParam, this.cleanDeviceData);
+        this.router.delete(`${this.path}/all`, admin, this.cleanAllDevices);
+        this.router.delete(`${this.path}/:id`, admin, checkIdParam, this.cleanDeviceData);
     }
 
     private getLatestReadingsFromAllDevices = async (request: Request, response: Response) => {
@@ -33,15 +35,27 @@ class DataController implements Controller {
         const { air } = request.body;
         const { id } = request.params;
 
-        const data = {
-            temperature: air[0].value,
-            pressure: air[1].value,
-            humidity: air[2].value,
-            deviceId: parseInt(id),
-            readingDate : new Date()
-        }
+        const schema = Joi.object({
+            air: Joi.array()
+                .items(
+                    Joi.object({
+                        id: Joi.number().integer().positive().required(),
+                        value: Joi.number().positive().required()
+                    })
+                )
+                .unique((a, b) => a.id === b.id),
+            deviceId: Joi.number().integer().positive().valid(parseInt(id, 10)).required()
+        });
 
         try {
+            const validatedData = await schema.validateAsync({air, deviceId: parseInt(id, 10)});
+            const data: IData = {
+                temperature: validatedData.air[0].value,
+                pressure: validatedData.air[1].value,
+                humidity: validatedData.air[2].value,
+                deviceId: validatedData.deviceId
+            }
+
             await this.dataService.createData(data);
             response.status(200).json(data);
         } catch (error) {
